d603060c8ca71d0ac8fce36a7c3b49f69b737455
JonthanAyala <20223tn085@utez.edu.mx>
2023-05-26 09:48:38 -0600

    [UPDATE] create message modify MYSQLConnection

---
+++ src/main/java/mx/edu/utez/firstapp/utils/Message.java
@@ -0,0 +1,10 @@
+package mx.edu.utez.firstapp.utils;
+
+public class Message<T> {
+
+    // new Message<MySQLConnection>();
+    T data;
+    String Message;
+    boolean error;
+    int status;
+}
--- src/main/java/mx/edu/utez/firstapp/utils/MySQLConnection.java
+++ src/main/java/mx/edu/utez/firstapp/utils/MySQLConnection.java
@@ -1,4 +1,4 @@
-package mx.edu.utez.firstapp;
+package mx.edu.utez.firstapp.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;

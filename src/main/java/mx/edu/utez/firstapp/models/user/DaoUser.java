9dc361cf856a2bebdb59c4bb61d0f9c5d0ee5c10
JonthanAyala <20223tn085@utez.edu.mx>
2023-05-31 11:03:02 -0600

    (31/05/23)[UPDATE] modfy DaoUser

--- src/main/java/mx/edu/utez/firstapp/models/user/DaoUser.java
+++ src/main/java/mx/edu/utez/firstapp/models/user/DaoUser.java
@@ -1,15 +1,51 @@
 package mx.edu.utez.firstapp.models.user;
 
 import mx.edu.utez.firstapp.models.crud.DaoRepository;
+import mx.edu.utez.firstapp.utils.MySQLConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DaoUser implements DaoRepository<User>{
+    private Connection conn;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+
 
 
     @Override
     public List<User> findAll() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try {
+
+         conn = new MySQLConnection().connect();
+         String query = "SELECT * from users;";
+         pstm = conn.prepareStatement(query);
+         rs = pstm.executeQuery();
+         while (rs.next()){
+             User user = new User();
+             user.setId(rs.getLong("id"));
+             user.setName(rs.getString("name"));
+             user.setLastname(rs.getString("lastname"));
+             user.setSurname(rs.getString("surname"));
+             user.setUsername(rs.getString("username"));
+             user.setBirthday(rs.getString("birthday"));
+             user.setStatus(rs.getString("status"));
+             users.add(user);
+         }
+
+        }catch (SQLException e){
+            Logger.getLogger(DaoUser.class.getName()).log(Level.SEVERE, "Error findAll"+e.getMessage());
+        }finally {
+            close();
+        }
+        return users;
     }
 
     @Override
@@ -31,4 +67,16 @@ public class DaoUser implements DaoRepository<User>{
     public boolean delete(Long id) {
         return false;
     }
+
+    public void close(){
+        try {
+            if(conn != null) conn.close();
+            if (pstm != null) pstm.close();
+            if (rs != null) rs.close();
+        }catch (SQLException e){
+
+        }
+
+    }
+
 }
